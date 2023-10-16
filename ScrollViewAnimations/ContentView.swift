302195894e67e3333f678f4dd099522b7f8ae75a
Elaidzha1940 <eli080293@gmail.com>
2023-10-16 16:25:12 +0300

    Updated CardItem

--- ScrollViewAnimations/ContentView.swift
+++ ScrollViewAnimations/ContentView.swift
@@ -10,10 +10,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    let cards = [CardItem(id: 0), CardItem(id: 1), CardItem(id: 2)]
+    
     var body: some View {
         
-        VStack {
+        ScrollView {
          
+            LazyHStack {
+                ForEach(cards) { card in
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(.ultraThickMaterial)
+                }
+            }
         }
         .preferredColorScheme(.dark)
     }
@@ -21,4 +29,9 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+
+struct CardItem: Identifiable {
+    var id: Int
 }
